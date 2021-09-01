6855c1244c79beb8bc86b147531dbe474881ab00
Stefan Oderbolz <oderbolz@gmail.com>
2021-09-01 15:11:13 +0200

    Add docopts
    Allow parameter to be set via cli

--- download.py
+++ download.py
@@ -4,7 +4,7 @@ from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
 
-def _download_request(url):
+def _download_request(url, verify=True):
     retry_strategy = Retry(
         total=3,
         backoff_factor=2,
@@ -16,30 +16,30 @@ def _download_request(url):
     http.mount("https://", adapter)
     http.mount("http://", adapter)
     headers = {'user-agent': 'Mozilla Firefox Mozilla/5.0; metaodi website-monitor at github'}
-    r = http.get(url, headers=headers, timeout=10)
+    r = http.get(url, headers=headers, timeout=10, verify=verify)
     r.raise_for_status()
     return r
 
 
-def download(url, encoding='utf-8'):
-    r = _download_request(url)
+def download(url, encoding='utf-8', verify=True):
+    r = _download_request(url, verify=verify)
     if encoding:
         r.encoding = encoding
     return r.text
 
 
-def download_content(url):
-    r = _download_request(url)
+def download_content(url, verify=True):
+    r = _download_request(url, verify=verify)
     return r.content
 
 
-def jsondownload(url):
-    r = _download_request(url, silent)
+def jsondownload(url, verify=True):
+    r = _download_request(url, verify=verify)
     return r.json()
 
 
-def download_file(url, path):
-    r = _download_request(url)
+def download_file(url, path, verify=True):
+    r = _download_request(url, verify=verify)
     with open(path, 'wb') as f:
         for chunk in r.iter_content(1024):
             f.write(chunk)
--- monitor.py
+++ monitor.py
@@ -1,31 +1,64 @@
+# -*- coding: utf-8 -*-
+"""Monitor changes on a website
+
+Usage:
+  monitor.py --url <url-of-website> [--selector <css-selector>] [--verbose] [--no-verify]
+  monitor.py (-h | --help)
+  monitor.py --version
+
+Options:
+  -h, --help                    Show this screen.
+  --version                     Show version.
+  -u, --url <url-of-website>    URL of the website to monitor.
+  -s, --selector <css-selector> CSS selector to check for changes [default: body]..
+  --verbose                     Option to enable more verbose output.
+  --no-verify                   Option to disable SSL verification for requests.
+
+"""
+
+
 import hashlib
 import time
 import logging
 from win10toast import ToastNotifier
 from bs4 import BeautifulSoup
+from docopt import docopt
 import download as dl
+
+arguments = docopt(__doc__, version='Monitor changes on a website 1.0')
+
+loglevel = logging.INFO
+if arguments['--verbose']:
+    loglevel = logging.DEBUG
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
-    level=logging.INFO,
+    level=loglevel,
     datefmt='%Y-%m-%d %H:%M:%S'
 )
+logging.captureWarnings(True)
 log = logging.getLogger(__name__)
 
-url = 'https://www.stadt-zuerich.ch/portal/de/index/politik_u_recht/amtliche_sammlung.html'
+url = arguments['--url']
+selector = arguments['--selector']
+verify = not arguments['--no-verify']
+
+if not verify:
+    import urllib3
+    urllib3.disable_warnings()
 
 old_hash = ''
 while True:
-    content = dl.download_content(url)
+    content = dl.download_content(url, verify=verify)
     soup = BeautifulSoup(content, 'html.parser')
-    as_list = soup.select_one('.mod_newsteaser')
+    as_list = soup.select_one(selector)
     log.debug(as_list.prettify())
     new_hash = hashlib.sha256(str(as_list).encode('utf-8')).hexdigest()
     log.info(f"Hash: {new_hash}")
     if old_hash != new_hash:
         log.info(f"Hash changed!")
         toast = ToastNotifier()
-        toast.show_toast("Amtliche Sammlung","Liste der letzten Einträge hat sich geändert", duration=20)
+        toast.show_toast("Website changed", "Content of css selector changed", duration=20)
     old_hash = new_hash
     time.sleep(30)
 
