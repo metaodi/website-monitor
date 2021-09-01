22f900a5433f847c60419110c424353eba6618b5
Stefan Oderbolz <oderbolz@gmail.com>
2021-09-01 11:55:28 +0200

    Use logging module for output

--- monitor.py
+++ monitor.py
@@ -1,8 +1,16 @@
 import hashlib
 import time
+import logging
 from win10toast import ToastNotifier
 from bs4 import BeautifulSoup
 import download as dl
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+log = logging.getLogger(__name__)
 
 url = 'https://www.stadt-zuerich.ch/portal/de/index/politik_u_recht/amtliche_sammlung.html'
 
@@ -11,11 +19,11 @@ while True:
     content = dl.download_content(url)
     soup = BeautifulSoup(content, 'html.parser')
     as_list = soup.select_one('.mod_newsteaser')
-    #print(as_list.prettify())
+    log.debug(as_list.prettify())
     new_hash = hashlib.sha256(str(as_list).encode('utf-8')).hexdigest()
-    print(f"Hash: {new_hash}")
+    log.info(f"Hash: {new_hash}")
     if old_hash != new_hash:
-        print(f"Hash changed!")
+        log.info(f"Hash changed!")
         toast = ToastNotifier()
         toast.show_toast("Amtliche Sammlung","Liste der letzten Einträge hat sich geändert", duration=20)
     old_hash = new_hash
