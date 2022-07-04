b47046f276b54aa03743b3f39505fe68af2e3599
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2022-07-04 15:14:37 +0200

    Use chrome options

--- download.py
+++ download.py
@@ -3,7 +3,7 @@ import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
