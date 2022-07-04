bba45b19f31f4a6c739bdc19dcb48151991eef75
Stefan Oderbolz <oderbolz@gmail.com>
2022-07-04 13:00:14 +0200

    Retry for 403 as well

--- download.py
+++ download.py
@@ -8,7 +8,7 @@ def _download_request(url, verify=True):
     retry_strategy = Retry(
         total=3,
         backoff_factor=2,
-        status_forcelist=[429, 500, 502, 503, 504],
+        status_forcelist=[403, 429, 500, 502, 503, 504],
         method_whitelist=["HEAD", "GET", "OPTIONS"]
     )
     adapter = HTTPAdapter(max_retries=retry_strategy)
