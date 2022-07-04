80770aa427dae8431406427a460652fa62fb7804
Stefan Oderbolz <oderbolz@gmail.com>
2022-07-04 20:31:58 +0200

    Create build_matrix.py

---
+++ build_matrix.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Check websites for changes
+
+Usage:
+  build_matrix.py --db <path-to-db-file>
+  build_matrix.py (-h | --help)
+  build_matrix.py --version
+
+Options:
+  -h, --help                    Show this screen.
+  --version                     Show version.
+  -d, --db <path-to-db-file>    URL of the website to monitor.
+"""
+
+
+import os
+import sys
+import time
+import sqlite3
+import traceback
+import json
+import requests
+from docopt import docopt
+
+
+conn = None
+try:
+    conn = sqlite3.connect(arguments['--db'])
+    conn.row_factory = sqlite3.Row
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM website")
+    rows = cur.fetchall()
+    config = [dict(r) for r in rows]
+    matrix = {'include': config}
+    print(json.dumps(matrix))
+
+except Exception as e:
+    print("Error: %s" % e, file=sys.stderr)
+    print(traceback.format_exc(), file=sys.stderr)
+    sys.exit(1)
