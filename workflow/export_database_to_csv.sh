8cc423d9fafbee7463c4073ed46b997d1f6bea41
Stefan Oderbolz <oderbolz@gmail.com>
2023-01-14 22:19:10 +0100

    Update export_database_to_csv.sh

--- workflow/export_database_to_csv.sh
+++ workflow/export_database_to_csv.sh
@@ -10,6 +10,8 @@ trap "cleanup" EXIT
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
+CSV_FILE=$1
+
 # export the SQLite to CSV
 sqlite3 -header -csv $DIR/website.db "select * from website;" > $DIR/temp.csv
-mv $DIR/temp.csv $DIR/../website.csv
+mv $DIR/temp.csv $DIR/../$CSV_FILE
