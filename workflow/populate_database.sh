a1bdf9697a93c4f17dc91e0f3ae2f8e0e171bae6
Stefan Oderbolz <oderbolz@gmail.com>
2023-01-14 22:13:19 +0100

    Update populate_database.sh

--- workflow/populate_database.sh
+++ workflow/populate_database.sh
@@ -9,7 +9,8 @@ function cleanup {
 trap "cleanup" EXIT
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
+CSV_FILE=$1
 
 # import the CSV to SQLite
 rm -rf $DIR/website.db
-sqlite3 $DIR/website.db -cmd '.mode csv' -cmd ".import $DIR/../website.csv website" .quit
+sqlite3 $DIR/website.db -cmd '.mode csv' -cmd ".import $DIR/../$CSV_FILE website" .quit
