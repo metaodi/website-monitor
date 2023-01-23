b873836c8b281c5fbb3df5686872e3a83941e521
Stefan Oderbolz <oderbolz@gmail.com>
2023-01-23 01:21:55 +0100

    Fix sort order to ignore case

--- workflow/export_database_to_csv.sh
+++ workflow/export_database_to_csv.sh
@@ -13,5 +13,5 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 CSV_FILE=$1
 
 # export the SQLite to CSV
-sqlite3 -header -csv $DIR/website.db "select * from website order by label asc;" > $DIR/temp.csv
+sqlite3 -header -csv $DIR/website.db "SELECT * FROM website ORDER BY label COLLATE NOCASE ASC;" > $DIR/temp.csv
 mv $DIR/temp.csv $DIR/../$CSV_FILE
